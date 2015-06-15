a42bb9b5da04f2d52bc8a582d09ae299494edb18
Rory Gibson <rory.gibson@brightnorth.co.uk>
2015-06-15 16:27:03 +0100

    PF-957 swap log4j for logback

--- src/leiningen/new/bn_webapp/project.clj
+++ src/leiningen/new/bn_webapp/project.clj
@@ -1,25 +1,23 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[ch.qos.logback/logback-core "1.1.3"]
+                 [ch.qos.logback/logback-classic "1.1.3"]
+                 [org.clojure/clojure "1.5.1"]
                  [ring/ring-core "1.2.0"]
                  [com.cemerick/friend "0.2.0" :exclusions [ring/ring-core]]
                  [compojure "1.1.5" :exclusions [ring/ring-core]]
                  [ring/ring-jetty-adapter "1.2.0"]
                  [dieter "0.4.1" :exclusions [ring/ring-core]]
                  [org.clojure/data.json "0.1.1"]
-                 [org.thymeleaf/thymeleaf "2.0.19"]                
+                 [org.thymeleaf/thymeleaf "2.0.19"]
                  [clj-time "0.5.1"]
                  [conf-er "1.0.1"]
-                 [org.slf4j/slf4j-simple "1.6.1"]
+                 [org.slf4j/slf4j-api "1.7.12"]
                  [mysql/mysql-connector-java "5.1.6"]
                  [korma "0.3.0-RC5"]
-                 [com.googlecode.flyway/flyway-core "2.2"]         
-                 [org.clojure/tools.logging "0.2.6"]
-                 [log4j/log4j "1.2.16" :exclusions [javax.mail/mail
-                                                    javax.jms/jms
-                                                    com.sun.jdmk/jmxtools
-                                                    com.sun.jmx/jmxri]]]
+                 [com.googlecode.flyway/flyway-core "2.2"]
+                 [org.clojure/tools.logging "0.3.1"]]
   :ring {:handler {{name}}.core/handler
          :init {{name}}.core/post-init!
          :port 4000}
